1f752af75e24366ea500793a93797d927aa9e568
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-10-22 22:48:41 +0530

    test : added a new test case to check if the fizzbuzz program can handle negative input also and return 'Buzz' if the number is divisible by 5

--- src/tests/fizzbuzz.test.js
+++ src/tests/fizzbuzz.test.js
@@ -19,4 +19,7 @@ describe("Test cases for FizzBuzz program", () => {
 	test('return "Fizz" if any negative number is divisible by 3', () => {
 		expect(fizzbuzz(-3)).toBe("Fizz");
 	});
+	test('return "Buzz" if negative number is divisible by 5', () => {
+		expect(fizzbuzz(-5)).toBe("Buzz");
+	});
 });
